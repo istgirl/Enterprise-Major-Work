f7b33f07162104c2a53582e4f825efaa0d00a1a2
istgirl <163796214+istgirl@users.noreply.github.com>
2025-04-18 01:42:57 +1000

    Added Navigation Bar

--- educationhomepagejava.js
+++ educationhomepagejava.js
@@ -1,23 +1,25 @@
-// Show user's first name
 window.addEventListener('DOMContentLoaded', () => {
     const email = localStorage.getItem('userEmail');
     if (email) {
-      const name = email.split('@')[0].split('.')[0]; // "nameerah"
-      const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
-      document.getElementById('userName').textContent = capitalized;
+        const name = email.split('@')[0].split('.')[0]; // "nameerah"
+        const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+        document.getElementById('userName').textContent = capitalized;
     }
-  });
+});
 
-  window.onbeforeunload = function () {
+window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-  };
-  
-  window.addEventListener("scroll", () => {
+};
+
+window.addEventListener("scroll", () => {
     const banner = document.getElementById("banner");
+    const stickyNav = document.getElementById("stickyNav");
+
     if (window.scrollY > 100) {
-      banner.classList.add("hidden");
+        banner.classList.add("hidden");
+        stickyNav.classList.add("show");
     } else {
-      banner.classList.remove("hidden");
+        banner.classList.remove("hidden");
+        stickyNav.classList.remove("show");
     }
-  });
-  
+});
