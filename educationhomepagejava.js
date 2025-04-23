8f03a6dad5e5a6219e66f555d64b7b4a2f4c3cb6
istgirl <163796214+istgirl@users.noreply.github.com>
2025-04-23 12:50:01 +1000

    memory game + edits

--- educationhomepagejava.js
+++ educationhomepagejava.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const email = localStorage.getItem('userEmail');
     if (email) {
-        const name = email.split('@')[0].split('.')[0]; // "nameerah"
+        const name = email.split('@')[0].split('.')[0]; 
         const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
         document.getElementById('userName').textContent = capitalized;
     }
